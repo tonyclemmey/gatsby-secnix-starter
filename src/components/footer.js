941767240a5427c5d595bdf40010f9ba5dfaa2a4
Tony Clemmey <tonyclemmey@gmail.com>
2019-03-16 00:13:09 +0000

    update files

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,8 +10,8 @@ const Footer = () => (
     © {new Date().getFullYear()}. Powered by
     {` `}
     <a href="https://www.secnix.com" target="_blank" rel="noopener noreferrer">Secnix</a><br /><br />
-    <a href="http://example.com/"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" className="mr-2" /></a>
-    <a href="http://example.com/"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" className="mr-2" /></a>
+    <a href="http://example.com/" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" className="mr-2" /></a>
+    <a href="http://example.com/" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" className="mr-2" /></a>
     <Link to="/contact"><FontAwesomeIcon icon={['fab', 'github']} size="2x" className="mr-2" /></Link>
     <Link to="/contact"><FontAwesomeIcon icon={['fas', 'home']} size="2x" className="mr-2" /></Link>
     <Link to="/contact"><FontAwesomeIcon icon={['fas', 'coffee']} size="2x" className="mr-2" /></Link>
