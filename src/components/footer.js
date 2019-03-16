06c0f7c1c1398f16e72d81fe5cfc6fa82a6a6139
Tony Clemmey <tonyclemmey@gmail.com>
2019-03-16 00:24:31 +0000

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,12 +10,12 @@ const Footer = () => (
     © {new Date().getFullYear()}. Powered by
     {` `}
     <a href="https://www.secnix.com" target="_blank" rel="noopener noreferrer">Secnix</a><br /><br />
-    <a href="http://example.com/" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" className="mr-2" /></a>
-    <a href="http://example.com/" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" className="mr-2" /></a>
-    <Link to="/contact"><FontAwesomeIcon icon={['fab', 'github']} size="2x" className="mr-2" /></Link>
-    <Link to="/contact"><FontAwesomeIcon icon={['fas', 'home']} size="2x" className="mr-2" /></Link>
-    <Link to="/contact"><FontAwesomeIcon icon={['fas', 'coffee']} size="2x" className="mr-2" /></Link>
-    <Link to="/contact"><FontAwesomeIcon icon={['fas', 'heart']} size="2x" className="mr-2" /></Link>
+    <a href="https://www.facebook.com/secnix/" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook']} size="2x" className="mr-2" /></a>
+    <a href="https://twitter.com/Secnix" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter']} size="2x" className="mr-2" /></a>
+    <a href="https://github.com/secnix" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} size="2x" className="mr-2" /></a>
+    <Link to="/"><FontAwesomeIcon icon={['fas', 'home']} size="2x" className="mr-2" /></Link>
+    <Link to="/about"><FontAwesomeIcon icon={['fas', 'coffee']} size="2x" className="mr-2" /></Link>
+    <Link to="/services"><FontAwesomeIcon icon={['fas', 'heart']} size="2x" className="mr-2" /></Link>
     <Link to="/contact"><FontAwesomeIcon icon={['fas', 'phone']} size="2x" className="mr-2" /></Link>
     <Link to="/contact"><FontAwesomeIcon icon={['fas', 'envelope']} size="2x" className="mr-2" /></Link>
     <Link to="/contact"><FontAwesomeIcon icon={['fas', 'map-marker-alt']} size="2x" className="mr-2" /></Link>
